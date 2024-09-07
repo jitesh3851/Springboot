4ba4d4b19a1b3bb5e547fd28962de6c4527b4c88
Jitesh Mishra <jitesh3851@gmail.com>
2024-09-07 23:06:29 +0530

    first-commit

---
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
@@ -0,0 +1,21 @@
+package com.in28minutes.rest.webservices.restful_web_services.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	
+	@RequestMapping(method = RequestMethod.GET, path="hello-world")
+	public String helloWorld() {
+		return "Hello World";
+	}
+	
+	@GetMapping(path = "hello-world2")
+	public String helloWorld2() {
+		return "Hello World2";
+	}
+
+}
