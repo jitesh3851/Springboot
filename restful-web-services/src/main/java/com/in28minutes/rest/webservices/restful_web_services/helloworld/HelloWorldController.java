53ddece5dfc8c90c2dc63564350396d15d35eb36
Jitesh Mishra <jitesh3851@gmail.com>
2024-09-08 00:08:53 +0530

    rest-apis for social media app

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.in28minutes.rest.webservices.restful_web_services.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,5 +18,18 @@ public class HelloWorldController {
 	public String helloWorld2() {
 		return "Hello World2";
 	}
+	
+	@GetMapping(path = "hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return  new HelloWorldBean("Hello World bean");
+	}
+	
+	
+	@GetMapping(path = "hello-world-bean/path-variable/{name}")
+	public HelloWorldBean helloWorlBeanPathVariable(@PathVariable String name) {
+		return  new HelloWorldBean("Hello World "+ name);
+	}
+	
 
 }
+		
