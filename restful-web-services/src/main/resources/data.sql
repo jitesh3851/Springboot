8e6fcc006ddff794f4fa45cab0e72d147a54e545
Jitesh Mishra <jitesh3851@gmail.com>
2024-09-11 23:20:38 +0530

    working with H2 database

---
+++ restful-web-services/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into user_details (id,birth_date,name) values(1000,current_date(),'Jitesh');
+insert into user_details (id,birth_date,name) values(1001,current_date(),'Kumar');
+insert into user_details (id,birth_date,name) values(1002,current_date(),'Mahesh');
+insert into user_details (id,birth_date,name) values(1003,current_date(),'Lokesh');
+insert into user_details (id,birth_date,name) values(1004,current_date(),'Jonathan');
+insert into user_details (id,birth_date,name) values(1005,current_date(),'Pratik');
