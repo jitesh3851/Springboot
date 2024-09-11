0c46cf6b43bd90c296bbbfb49629daade7835ac6
Jitesh Mishra <jitesh3851@gmail.com>
2024-09-11 23:41:31 +0530

    implemented get/post/delete apis with JpaRepository

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/User.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/User.java
@@ -18,13 +18,17 @@ public class User {
 	private Integer id;
 	
 	@Size(min = 2, message = "Name should be atlest 2 charecters long")
-	@JsonProperty("user_name")
+	//@JsonProperty("user_name")
 	private String name;
 	
 	@Past(message = "Birthdate should be in the past")
-	@JsonProperty("birth_date")
+	//@JsonProperty("birth_date")
 	private LocalDate birthDate;
 	
+	public User() {
+		super();
+	}
+
 	public User(Integer id, String name, LocalDate birthDate) {
 		super();
 		this.id = id;
---
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/UserJpaResource.java
@@ -0,0 +1,61 @@
+package com.in28minutes.rest.webservices.restful_web_services.user;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.in28minutes.rest.webservices.restful_web_services.jpa.UserRepository;
+
+import jakarta.validation.Valid;
+
+@RestController
+public class UserJpaResource {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	@GetMapping(path="/jpa/users")
+	public List<User> retrieveAllUsers(){
+		return repository.findAll();
+	}
+	
+	@GetMapping(path="/jpa/users/{userId}")
+	public EntityModel<User> getUser(@PathVariable int userId) {
+		 Optional<User> user = repository.findById(userId);
+		 if(user.isEmpty()) {
+			 throw new UserNotFound("userId:"+userId);
+		 }
+		 EntityModel<User> entityModel = EntityModel.of(user.get());
+		 WebMvcLinkBuilder link = WebMvcLinkBuilder.linkTo(
+				 WebMvcLinkBuilder.methodOn(this.getClass()).retrieveAllUsers());
+		 entityModel.add(link.withRel("all-users"));
+		return entityModel;
+	}
+	
+	@PostMapping(path="/jpa/users")
+	public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
+		 User createdUser = repository.save(user);
+		 URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+		 .path("/{userId}").buildAndExpand(createdUser.getId()).toUri();
+		 ResponseEntity<User> responseEntity = ResponseEntity.created(uri).build();
+		 return responseEntity;
+	}
+	
+	@DeleteMapping(path="/jpa/users/{userId}")
+	public void deleteUser(@PathVariable int userId) {
+		repository.deleteById(userId);
+	}
+
+}
