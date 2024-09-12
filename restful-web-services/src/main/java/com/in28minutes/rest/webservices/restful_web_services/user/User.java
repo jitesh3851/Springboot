84cffaa986bb22055d7d683838f850e8b2c1dea5
Jitesh Mishra <jitesh3851@gmail.com>
2024-09-12 19:54:39 +0530

    creating Post enity with mapping with User entity

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/User.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/User.java
@@ -1,12 +1,14 @@
 package com.in28minutes.rest.webservices.restful_web_services.user;
 
 import java.time.LocalDate;
+import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
@@ -24,6 +26,10 @@ public class User {
 	@Past(message = "Birthdate should be in the past")
 	//@JsonProperty("birth_date")
 	private LocalDate birthDate;
+	
+	@OneToMany(mappedBy = "user")
+	@JsonIgnore
+	private List<Post> posts;
 	
 	public User() {
 		super();
@@ -60,9 +66,19 @@ public class User {
 		this.birthDate = birthDate;
 	}
 
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
+
 	@Override
 	public String toString() {
 		return "User [id=" + id + ", name=" + name + ", birthDate=" + birthDate + "]";
 	}
 
+	
+	
 }
--- restful-web-services/src/main/resources/data.sql
+++ restful-web-services/src/main/resources/data.sql
@@ -4,3 +4,10 @@ insert into user_details (id,birth_date,name) values(1002,current_date(),'Mahesh
 insert into user_details (id,birth_date,name) values(1003,current_date(),'Lokesh');
 insert into user_details (id,birth_date,name) values(1004,current_date(),'Jonathan');
 insert into user_details (id,birth_date,name) values(1005,current_date(),'Pratik');
+
+insert into post (id, description, user_id) values (2000,'I want to learn AWS',1000);
+insert into post (id, description, user_id) values (2001,'I want to learn Angular',1001);
+insert into post (id, description, user_id) values (2002,'I want to learn DevOps',1002);
+insert into post (id, description, user_id) values (2003,'I want to learn DataS tructure',1003);
+insert into post (id, description, user_id) values (2004,'I want to learn Java 17',1004);
+insert into post (id, description, user_id) values (2005,'I want to learn Design patterns',1005);
